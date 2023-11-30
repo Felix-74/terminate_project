68b93a75ff66431fdbcc9fc3a33ff9ad36e006d5
Игорь Теплов <2173045@mail.ru>
2023-11-30 17:46:45 +0500

    create list_items

--- main.py
+++ main.py
@@ -6,5 +6,16 @@ app = FastAPI()
 @app.get("/")
 def hello_index():
     return {
-        "message": "Hello index!",
+        "message": "Hello my friends!",
     }
+
+@app.get("/items/")
+def list_items():
+    return [
+        "Item1",
+        "Item2",
+        "Item3",
+    ]
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", reload=True)
