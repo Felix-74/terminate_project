6b62e854bc516f7eb8ba101df4ed2c6f750321ba
Игорь Теплов <2173045@mail.ru>
2023-12-05 11:32:40 +0500

    Check #3

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import uvicorn
-from fastapi import FastAPI,Body
+from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
+
 class CreateUser(BaseModel):
     email: EmailStr
+
 
 @app.get("/")
 def hello_index():
@@ -14,12 +16,22 @@ def hello_index():
     }
 
 
+@app.get("/calc/add/")
+def add(a: int, b: int):
+    return {
+        "a": a,
+        "b": b,
+        "result": a + b,
+    }
+
+
 @app.post("/users/")
-def create_user(email: EmailStr=Body()):
+def create_user(user: CreateUser):
     return {
         "message": "success",
-        "email": email,
+        "email": user.email,
     }
+
 
 @app.get("/hello/")
 def hello(name: str = "Igor"):
@@ -29,6 +41,7 @@ def hello(name: str = "Igor"):
 def get_latest_item():
     return {"item": {"id": "0", "name": "latest"}}
 
+
 @app.get("/items/")
 def list_items():
     return [
@@ -36,6 +49,7 @@ def list_items():
         "Item2",
         "Item3",
     ]
+
 
 @app.get("/items/{item_id}/")
 def get_item_by_id(item_id: int):
