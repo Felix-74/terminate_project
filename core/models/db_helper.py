02f44b744bb383b3d87b357cf797e738a89ecd79
Игорь Теплов <2173045@mail.ru>
2023-12-08 19:22:35 +0500

    Add db_helper.py and core

---
+++ core/models/db_helper.py
@@ -0,0 +1,10 @@
+from asyncio import current_task
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    create_async_engine,
+    async_sessionmaker,
+    async_scoped_session,
+)
+
+from core.config import settings
